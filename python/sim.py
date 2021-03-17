072a30807bea52e44a16233c3ad5af61142d2cd9
Victoria Ludwig <vfludwig@mail.lipscomb.edu>
2021-03-17 11:46:12 -0500

    Added details to sim.py
    -created index.html

--- python/sim.py
+++ python/sim.py
@@ -12,3 +12,18 @@ fluInfectionRate = 1.3
 fluIncubationTime = 4
 fluHospitalizationRate = .02
 fluFatalityRate = .01
+
+totalPopulation = #input
+diseaseType = #input
+
+daycount = 0
+infected = 0
+recovered = 0
+
+class Person:
+    def __init__(self):
+        self.infected = 0
+
+def beginSim():
+    for x in range(totalPopulation):
+        #do stuff
