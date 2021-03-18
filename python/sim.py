2b64147e66fb943ad6fc4f1b124d96d2d677d8f9
Victoria Ludwig <vfludwig@mail.lipscomb.edu>
2021-03-18 09:27:51 -0500

    Comments

--- python/sim.py
+++ python/sim.py
@@ -3,6 +3,7 @@ import random
 import statistics
 
 #all data are the high ends of information from the CDC
+###change to dictionary
 covidInfectionRate = 2.5
 covidIncubationTime = 14
 covidHospitalizationRate = .19
@@ -13,12 +14,13 @@ fluIncubationTime = 4
 fluHospitalizationRate = .02
 fluFatalityRate = .01
 
-totalPopulation = #input
-diseaseType = #input
+totalPopulation = ###input from website
+diseaseType = ###input from website
 
 daycount = 0
 infected = 0
 recovered = 0
+###possibly more details
 
 class Person:
     def __init__(self):
@@ -26,4 +28,4 @@ class Person:
 
 def beginSim():
     for x in range(totalPopulation):
-        #do stuff
+        ###do stuff
